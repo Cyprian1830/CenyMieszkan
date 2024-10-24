0fdc023719ad0475d30dd7fc5e21cfbd9743b17f
Cyprian Szot <cyprian1830@gmail.com>
2024-10-24 12:38:58 +0200

    wykonane przypisywanie1

---
+++ src/zajecia02/przypisywania.py
@@ -0,0 +1,14 @@
+#1
+dane = (2024, 'Python', 3.8)
+rok = dane[0]
+jezyk = dane[1]
+wersja = dane[2]
+print(f"Rok: {rok}, Jezyk: {jezyk}, wersja: {wersja}")
+
+#2
+oceny = [4, 3, 5, 2, 5, 4]
+pierwsza,*srodek,ostatnia = oceny
+print(pierwsza, srodek, ostatnia)
+
+#3
+info = ('Jan', 'Kowalski', 30, 'Polska', 'programista')
