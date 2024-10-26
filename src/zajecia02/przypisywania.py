ba2f28177ab6fb997be2b48ce89aa5d2721cae3d
Cyprian Szot <cyprian1830@gmail.com>
2024-10-26 19:02:39 +0200

    Gotowe zadania z zajęć nr 2

--- src/zajecia02/przypisywania.py
+++ src/zajecia02/przypisywania.py
@@ -1,14 +1,81 @@
-#1
+# 1
 dane = (2024, 'Python', 3.8)
 rok = dane[0]
 jezyk = dane[1]
 wersja = dane[2]
 print(f"Rok: {rok}, Jezyk: {jezyk}, wersja: {wersja}")
 
-#2
+# 2
 oceny = [4, 3, 5, 2, 5, 4]
-pierwsza,*srodek,ostatnia = oceny
+pierwsza, *srodek,ostatnia = oceny
 print(pierwsza, srodek, ostatnia)
 
-#3
+# 3
 info = ('Jan', 'Kowalski', 30, 'Polska', 'programista')
+imie, nazwisko, _, _, zawod = info
+print(f"Imię: {imie}, Nazwisko: {nazwisko}, zawod: {zawod}")
+
+# 4
+dane = (2024, ['Python', 3.8, ('Stabilna', 'Wersja')])
+rok = dane[0]
+jezyk = dane[1][0]
+wersja = dane[1][1]
+opis = dane[1][2]
+print(f"Rok: {rok}, jezyk: {jezyk}, wersja: {wersja}, opis: {opis}")
+
+# Przypisania z wieloma celami i współdzielone referencje
+a = [1, 2, 3]
+b = a
+b[0] = 'zmieniono'
+print(a)
+print(b)
+# Każda zmienna w Pythonie to obiekt, a zmienne to tylko referencje które w naszym przypadku
+# wskazują na ten sam obiekt, czyli przy modyfikacji jednej wartości w referencji b
+# zmienia się też wartość dla zmiennej a
+
+#2
+print("##########")
+c = list(a)
+c[0] = 'nowa wartość'
+print(a)
+print(b)
+print(c)
+
+# kopiowanie listy zapobiegło zmianie wartości listy a i b, podczas zmiany listy c
+# ze względu na fakt, że c nie wskazuje na to samo miejsce w pamieci co zmienne a i b
+
+#3
+x = y = 10
+y = y + 1
+print(x)
+print(y)
+
+# Dzieje się tak ze względu na fakt, że zmienne integer są niemutowalne, czyli podczas
+# zmiany ich wartości tworzy się nowy obiekt.Sprawia to, że po dodaniu 1 do y, referencje x i y
+# wskazują od tej pory na inne miejsce w pamięci, co sprawia że x nie zmienia swej wartości
+
+# Przypisania rozszerzone i współdzielone referencje
+
+K = [1, 2]
+L = K
+# konkatenacja
+K = K + [3, 4]
+M = [1, 2]
+N = M
+# przypisanie rozszerzone
+M += [3, 4]
+print("K:", K);
+print("L:", L);
+print("M:", M);
+print("N:", N);
+
+
+# Zawartość listy K i L różnia się ze względu na to,iż konkatenacja utworzy nowy
+# obiekt K, przez co nie zostanie zmodyfikowany obiekt L
+# Inaczej będzie w przypadku przypisania rozszerzonego, gdyż jest to mechanizm dzieki ktoremu
+# nie zostanie utworzona nowa referencja do obiektu, czyli przez operacje +=[3,4]
+# zarowno obiekt M i N zostanie zmodyfikowany.
+
+
+
+
---
+++ src/zajecia02/pętle.py
@@ -0,0 +1,122 @@
+# Pętle z licznikami
+
+for i in range(6):
+    print(f" Licznik: {i}")
+
+# Skanowanie sekwencji
+
+owoce = ['jabłko', 'banan', 'pomarańcza']
+for owoc in owoce:
+    print(owoc)
+
+# Przetasowanie sekwencji
+# range(len(x)) iteruje po indeksach sekwencji.
+# Może być użyte do manipulacji elementami na podstawie ich indeksów.
+
+owoce = ['jabłko', 'banan', 'pomarańcza']
+for i in range(len(owoce)):
+    owoce[i] = owoce[i].upper()
+print(owoce)
+
+
+# Przechodzenie niewyczerpujące
+liczby = [1, 2, 3, 4, 5, 6]
+for i in range(0, len(liczby), 2):
+    print(liczby[i])
+
+# Alternatywnie, z użyciem slicing
+print(liczby[::2])
+
+# Listy składane
+liczby = [1, 2, 3, 4, 5, 6]
+parzystosc = ['parzysta' if liczba % 2 == 0 else 'nieparzysta' for liczba in liczby]
+print(parzystosc)
+
+# Przechodzenie równoległe
+
+imiona = ['Anna', 'Jan', 'Piotr']
+wieki = [25, 30, 35]
+for imie, wiek in zip(imiona, wieki):
+    print(f"{imie} ma {wiek} lat")
+
+
+def kwadrat(liczba):
+    return liczba ** 2;
+
+
+liczby = [1, 2, 3, 4]
+kwadraty = list(map(kwadrat, liczby))
+print(kwadraty)
+
+# zad 1
+
+"""
+Mając dwie listy, imiona = ['Anna', 'Jan', 'Ewa'] i oceny = [5, 4, 3], 
+użyj zip do stworzenia pary każdego imienia z odpowiadającą mu oceną. 
+Następnie, iteruj przez te pary, wyświetlając imię wraz z oceną. 
+Co się stanie, jeśli listy będą miały różne długości?
+"""
+imiona = ['Anna', 'Jan', 'Ewa', "Ala"]
+oceny = [5, 4, 3]
+
+para = list(zip(imiona, oceny))
+print(para)
+
+for imie, ocena in zip(imiona, oceny):
+    print(f"{imie} dostala ocene: {ocena}")
+
+# W momencie w którym np. lista imion jest dłuższa od listy ocen podczas iteracji
+# ostatnie imie zostanie pominiete(ogolnie rzecz biorąc ostatnia para imie, ocena
+# zostanie pominięte ze względu na brak oceny)
+
+# zad 2
+# Mając listę liczby = [1, 2, 3, 4, 5], napisz funkcję kwadrat(x),
+# która zwraca kwadrat liczby x. Użyj map z tą funkcją,
+# aby stworzyć nową listę, w której każdy element jest kwadratem
+# odpowiadającego mu elementu z listy liczby. Wyświetl tą listę.
+
+liczby = [1, 2, 3, 4, 5]
+
+
+def kwadrat(x):
+    return x**2
+
+
+kwadratowe_liczby = list(map(kwadrat, liczby))
+print(kwadratowe_liczby)
+
+
+# Iteratory
+liczby = [1, 2, 3]
+it = iter(liczby)
+print(next(it))
+print(next(it))
+print(next(it))
+print("##################")
+# Zadanie dodatkowe
+
+
+class FibonacciIterator:
+    def __init__(self, max_elements):
+        self.max_elements = max_elements
+        self.index = 0
+        self.a, self.b = 0, 1
+
+    def __iter__(self):
+        return self  # Iterator zwraca sam siebie
+
+    def __next__(self):
+        if self.index >= self.max_elements:
+            raise StopIteration
+    # Zatrzymanie iteracji, gdy limit zostanie przekroczony
+
+        fib_number = self.a
+        self.a, self.b = self.b, self.a + self.b  # Aktualizuje wartości dla kolejnych wyrazów
+        self.index += 1
+        return fib_number
+
+
+fib_iterator = FibonacciIterator(10)
+# Przykładowo: generuje 10 elementów ciągu Fibonacciego
+for num in fib_iterator:
+    print(num)
